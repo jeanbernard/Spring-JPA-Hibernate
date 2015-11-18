05061f4d10e9655de24e3c347bce0af0b0a63aa2
Jean Bernard <jeanmarinobernard@gmail.com>
2015-11-18 08:44:19 -0400

    First commit.

--- src/main/java/com/pluralsight/model/Goal.java
+++ src/main/java/com/pluralsight/model/Goal.java
@@ -2,9 +2,17 @@ package com.pluralsight.model;
 
 import org.hibernate.validator.constraints.Range;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 
+@Entity
 public class Goal {
+
+	@Id
+	@GeneratedValue
+	private long id;
 
 	@Range(min = 1, max = 120)
 	private int minutes;
